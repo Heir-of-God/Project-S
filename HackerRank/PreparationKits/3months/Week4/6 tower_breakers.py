98e2e7784d577218a359a3a5a868537781473974
Heir-of-God <140493860+Heir-of-God@users.noreply.github.com>
2024-06-30 09:51:02 +0300

    Solved HackerRank 3month preparation kit problem N. 6 (Week4)

---
+++ HackerRank/PreparationKits/3months/Week4/6 tower_breakers.py
@@ -0,0 +1,19 @@
+import os
+
+
+def towerBreakers(towers: int, height: int) -> int:
+    if towers & 1 == 1 and height != 1:
+        return 1
+    return 2
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+    t = int(input().strip())
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+        n = int(first_multiple_input[0])
+        m = int(first_multiple_input[1])
+        result = towerBreakers(n, m)
+        fptr.write(str(result) + "\n")
+    fptr.close()
