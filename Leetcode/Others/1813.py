1206e0cdf2ac5abb424728a9838f09eaec76745a
Heir-of-God <140493860+Heir-of-God@users.noreply.github.com>
2024-08-13 09:54:43 +0300

    Solved leetcode problem 1813. Sentence Similarity III

---
+++ Leetcode/Others/1813.py
@@ -0,0 +1,25 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        s1 = sentence1.split()
+        s2 = sentence2.split()
+        if len(s1) == 1:
+            return s1[0] in (s2[0], s2[-1])
+        elif len(s2) == 1:
+            return s2[0] in (s1[0], s1[-1])
+
+        left = 0
+        right1 = len(s1) - 1
+        right2 = len(s2) - 1
+
+        while left <= right1 and left <= right2:
+            if s1[left] != s2[left] and s1[right1] != s2[right2]:
+                return False
+
+            if s1[left] == s2[left]:
+                left += 1
+
+            if s1[right1] == s2[right2]:
+                right1 -= 1
+                right2 -= 1
+
+        return True
